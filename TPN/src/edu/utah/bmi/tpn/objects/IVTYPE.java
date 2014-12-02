f358b24dee598d5a2b27ff4c7ef062ac75e2c0c9
Jianlin Shi <Jianlin Shi@brokenjade-ThinkPad-L530>
2014-12-02 15:53:19 -0700

    Add ivtype

---
+++ TPN/src/edu/utah/bmi/tpn/objects/IVTYPE.java
@@ -0,0 +1,8 @@
+package edu.utah.bmi.tpn.objects;
+
+public final class IVTYPE {
+	public final static int CentralLine = 1;
+	public final static int PeripheralLine = 0;
+	public final static int Deep=2;
+	public final static int NotSpecified = -1;
+}
